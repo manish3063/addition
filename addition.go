8c846dd35155ca6d080f99bc4c19ab544952f9df
manish3063 <manishsingh111095@gmail.com>
2022-06-08 15:54:47 +0530

    addition

---
+++ addition.go
@@ -0,0 +1,12 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 15
+	num2 := 10
+	result := num1 + num2
+	fmt.Println("result is: ", result)
+}
